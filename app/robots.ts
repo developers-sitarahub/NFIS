d02c04eaf13b84dd6303e17151604eac0b9dd77b
developers-sitarahub <developers@sitarahub.com>
2026-03-26 17:02:54 +0530

    feat: implement robots.txt to manage web crawler access and sitemap.

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,17 @@ export default function robots(): MetadataRoute.Robots {
   const SITE_URL = 'https://nationalfranchiseinvestmentsummit.com';
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/_next/static/'],
+        disallow: ['/api/', '/_next/'],
+      },
+      {
+        userAgent: 'Google-Extended',
+        allow: '/',
+      }
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
